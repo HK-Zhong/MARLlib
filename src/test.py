fe30b69749c97a7cd5e13d28bb203c66a8170795
hk-zhong <786486234@qq.com>
2026-01-21 21:25:44 +0800

    修改为torch1.9纯CPU版本

--- src/test.py
+++ src/test.py
@@ -1,13 +1,25 @@
+import os
 from marllib import marl
 
+os.environ["CUDA_VISIBLE_DEVICES"] = ""
+
 # prepare env
-env = marl.make_env(environment_name="mpe", map_name="simple_spread", force_coop=True)
+env = marl.make_env(environment_name="mpe",
+                    map_name="simple_spread",
+                    force_coop=True)
 
 # initialize algorithm with appointed hyperparameters
 mappo = marl.algos.mappo(hyperparam_source='mpe')
 
 # build agent model based on env + algorithms + user preference
-model = marl.build_model(env, mappo, {"core_arch": "mlp", "encode_layer": "128-256"})
+model = marl.build_model(env, mappo,
+                         {"core_arch": "mlp", "encode_layer": "128-256"})
 
 # start training
-mappo.fit(env, model, stop={'timesteps_total': 100000}, share_policy='group', checkpoint_freq=100)
+mappo.fit(env, model,
+          stop={'timesteps_total': 100000},
+          share_policy='group',
+          checkpoint_freq=100,
+          num_workers=8,
+          num_gpus=0,
+          num_gpus_per_worker=0)
--- src/visualization.py
+++ src/visualization.py
@@ -1,5 +1,7 @@
 from marllib import marl
+import os
 
+os.environ["CUDA_VISIBLE_DEVICES"] = ""
 # prepare the environment
 env = marl.make_env(environment_name="mpe", map_name="simple_spread", force_coop=True)
 
@@ -11,9 +13,12 @@ model = marl.build_model(env, mappo, {"core_arch": "mlp", "encode_layer": "128-2
 
 # rendering
 mappo.render(env, model,
-             restore_path={'params_path': "examples/checkpoint/params.json",  # experiment configuration
-                           'model_path': "examples/checkpoint/checkpoint-6250",  # checkpoint path
+             restore_path={'params_path': "exp_results/mappo_mlp_simple_spread/MAPPOTrainer_mpe_simple_spread_9c90d_00000_0_2026-01-21_20-49-23/params.json",  # experiment configuration
+                           'model_path': "exp_results/mappo_mlp_simple_spread/MAPPOTrainer_mpe_simple_spread_9c90d_00000_0_2026-01-21_20-49-23/checkpoint_000032/checkpoint-32",  # checkpoint path
                            'render': True},  # render
              local_mode=True,
              share_policy="all",
-             checkpoint_end=False)
+             checkpoint_end=False,
+             num_gpus=0,
+             num_gpus_per_worker=0
+             )
